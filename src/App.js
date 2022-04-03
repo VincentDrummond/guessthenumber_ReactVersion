5cd6a7770c76f4a93819a371826a34bb1f20750a
Vincent Drummond <vincentdrummond@hotmail.co.uk>
2022-04-03 12:56:48 +0100

    final styled version, some inputs not working as intended

--- src/App.js
+++ src/App.js
@@ -4,59 +4,191 @@
 import { Component } from 'react';
 import View from "./View.js";
 // bring in View from another file
-import { randomNumber } from './components/maths.js';
 import { randomInteger } from './components/maths.js';
 
 
 
 class App extends Component {
 //use class if there is a need to handle state
-	static TOO_LOW = Symbol('too low');
-	static HIGH = Symbol('too high');
-	static EXACT_MATCH = Symbol('exact match');
-	static OUT_OF_BOUNDS = Symbol('out of bounds');
-	static GAME_OVER = Symbol('game over');
+	static DIFFICULTIES = {
+// static doesn't change per instance
+// DIFFICULTIES will always be what it is here
+		easy: {
+			tries: 10,
+			upper: 25
+		},
+		medium: {
+			tries: 7,
+			upper: 50,
+		},
+		hard: {
+			tries: 5,
+			upper: 100,
+		}
+	}
 
 	state = {
-		query: " ", settings:{}
+		difficulty: null,
+		lower: null,
+		upper: null,
+		maxTries: null,
+		target: null,
+		attempts: [],
+		guess: null,
 	};
-// the query in the input
+
+	get attemptsRemaining() {
+		return this.state.maxTries - this.state.attempts.length;
+	}
 
 	render() {
 		return (
+// return everything inside View from View.js
+
+// this is sending all these values to the View
+// functional component in View.js as props, and then destructuring them in line 12
 				<View
-					onClick={() => this.numberGuess()}
-					onChange={event => this.onChange(event)}
-					/>
-		)
-// return the View from View.js
-// pass in the event listeners/handles (methods attached the to the class)
+					guess={this.state.guess}
+					difficulty={this.state.difficulty}
+					
+					lower={this.state.lower}
+					upper={this.state.upper}
+					attempts={this.state.attempts}
+					attemptsLeft={this.attemptsRemaining}
+
+					onInitialise={({ target: { options, selectedIndex } }) => {
+// destructed e here, then destructed target as is an object
+// pulled out the options & selectedIndex attributes from the target
+						this.onInitialise(options[selectedIndex].value);
+// this is then pushed into the intialise method
+					}}
+
+// selectedIndex gives you the index of the selected value
+// ie. 0, 1 or 2 as are 3 options(easy, medium, hard)
+
+				/*	ANOTHER WAY OF DOING THE ABOVE
+					initialise={e => {
+						let selectDropDown = e.target;
+							- gets the selectbox HTML element
+						
+						let index = selectDropDown.selectedIndex;
+							- gets the index of selectDropDown(the HTML element)
+
+						let difficulty = selectDropDown.options[index].value;
+							- go to thw options array, pass in the index
+								and get the value of the index
+
+						alert(difficulty);
+							- alert the value of the index of the selected element
+				*/
+
+					onGuessButton={() => {
+						this.guess(parseInt(this.state.guess));
+					}}
+
+					onResetButton={() => {
+						this.reset();
+					}}
+
+					onHelpButton={e => {
+						this.help();
+					}}
+
+					onInput={({ target: { value: guess } }) => {
+// destructure to get value from target (the guess input box), and rename it guess
+						this.setState({ guess });
+// sets the set to take the guess value
+					}}
+
+				/>
+		);
 	}
 
-	// getHelp() {
-	// 	console.log("Help Me");
-	// }
-
-	numberGuess() {
-		alert(this.state.query);
-// grabs the state containing the query in line 36
-	}
-
-
-
-	onChange(event) {
-		let input = event.target;
-		let value = input.value;
+	onInitialise(difficulty = "easy") {
+			const {
+				lower = 1,
+				upper = 100,
+				tries: maxTries = 10,
+			} = App.DIFFICULTIES[difficulty];
 
 		this.setState({
-			query: value,
+// sets the state on initialise from null to what is defined in the difficulties parameters
+			difficulty,
+			lower,
+			upper,
+			maxTries,
+			target: randomInteger(lower, upper)
+
 		})
-
-		
-// to get the value of the input query, insert it into state
 	}
+
+	guess(number = 1) {
+		this.setState({
+			guess: ""
+		});
+
+		if (this.attemptsRemaining === 0) {
+			alert("That's game over!");
+
+			this.setState({
+				difficulty: null,
+			})
+		}
+
+		const {
+			target,
+			upper,
+			lower,
+			attempts
+		} = this.state;
+// destructures this.state to avoid typing it multiple below
+
+		if (number === target) {
+			alert("You win");
+
+			this.setState({
+				difficulty: null,
+			})
+		}
+
+		if (number < lower || number > upper) {
+			alert("Choose a number between the bounds");
+		} else {
+			this.setState({
+				attempts: [...attempts, number]
+			})	
+		if (number < target) {
+			alert("Your guess is too low");
+		}
+	}
+
+		if (number > target) {
+			alert("Your guess is too high")
+		}
+
+		if (this.state.attempts.includes(number)) {
+			return alert("You've already guessed that number'")
+		}
+	}
+
+	help() {
+		const lastGuess = this.state.attempts[this.state.attempts.length -1];
+							// call the method at = returns the value at the last item in the list
+							// -1 counts backwards from the end of the array
+
+		if (isNaN(lastGuess))
+			alert("You need to make a guess first");
+
+		let answerDistance = Math.ceil(Math.abs(this.state.target - lastGuess) / 10) * 10;
+			alert(`You are within ${ answerDistance } of the answer.`)
+	}
+
+	reset() {
+		this.setState({
+			difficulty: null,
+		})
+	}
+
 }
-
-
 
 export default App;
--- src/View.js
+++ src/View.js
@@ -5,21 +5,82 @@
 // dont use too much logic - pass down functionality from the App
 // App component imports in from here to render the UI
 
+// functional components DO NOT need imports
+// (unless you're using state)
+// all you're doing is making a function
 
-export default function View(props) {
-//use functional if there is no need to handle state
-    return (
+export default function View({
+//  use functional components if there is no need to handle state
+// this has been destructured from props
+// so now props. can be deleted
+    difficulty,
+    guess,
+    attempts,
+
+    lower,
+    upper,
+    attemptsLeft,
+
+    onGuessButton,
+    onHelpButton,
+    onInitialise,
+    onResetButton,
+    onInput
+}) {
+
+    if (difficulty) {
+        return (
         <>
-        <h1>Guess The Number</h1>
-        <h2>Try to guess a number between 1 and 100 in less than 10 tries</h2>
+            <header>
+                <h1>Guess The Number</h1>
+            </header>
 
-        <input  type="text"
-                placeholder="?"
-                onChange={props.onChange}/>
-                
-        <button onClick={props.onClick}>Guess</button>
+                <div className="gameBoard">
+                    <p className="selectedMode">Difficulty Mode: <span>&nbsp;{difficulty}</span></p>
+                    <div className="info-text">
+                            <p>Guess range: <span>{lower}</span> to <span>{upper}</span></p>
+                            <p>Tries left: <span>{attemptsLeft}</span></p>
+                    </div>
 
-        {/* <button onClick={props.getHelp}>Help</button> */}
+                                <input  onChange={onInitialise}
+                                        type="number"
+                                        placeholder="?" 
+                                        value={guess} />
+
+                        <div className="buttonsSection">
+                                <button onClick={onHelpButton}>Help</button>
+                                <button onClick={onGuessButton}>Guess</button>
+                                <button onClick={onResetButton}>Reset</button>
+                        </div>
+                </div>
+
+            <footer>
+                <p>Built by G. Robertson &amp; V. Drummond</p>
+            </footer>
+
         </>
-    );
-}
+
+        );
+    } else {
+        return (
+        <>
+            <header>
+                <h1>Guess The Number</h1>
+            </header>
+
+            <div className="gameBoard">
+                <select onChange={onInitialise}>
+                    <option>Choose a difficulty level</option>
+                    <option value="easy">Go easy on me</option>
+                    <option value="medium">This ain't my first rodeo</option>
+                    <option value="hard">Pain is good</option>
+                </select>
+            </div>
+
+            <footer>
+                <p>Built by G. Robertson &amp; V. Drummond</p>
+            </footer>
+        </>
+        )
+    }
+    }
